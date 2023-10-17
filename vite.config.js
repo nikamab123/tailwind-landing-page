8f1ea0ecb0bfb4dfc1ebd987edddebb25dda33ac
nikamab123 <71548135+nikamab123@users.noreply.github.com>
2023-10-17 22:47:17 +0530

    6

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/tailwind-landing-page",
+
   plugins: [react()],
   build: {
     outDir: 'build', // or any other directory name you prefer
