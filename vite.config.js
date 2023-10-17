6c385c2cdd510337a6f57bbba8c03257f86f09f4
nikamab123 <71548135+nikamab123@users.noreply.github.com>
2023-10-17 22:41:00 +0530

    5

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir: 'build', // or any other directory name you prefer
+  },
+  publicDir: 'public', // or any other directory name you prefer
 })
